e184ee328774deaf6c2b5f9240ade5b0ffbc2e62
Hariokas <Haroldas.Zalg@gmail.com>
2023-05-15 22:55:34 +0300

    Implemented VolumeMixerPage

--- Media Controller Remote Client/HamburgerMenuPages/VolumeMixerPage.xaml.cs
+++ Media Controller Remote Client/HamburgerMenuPages/VolumeMixerPage.xaml.cs
@@ -1,9 +1,28 @@
+using Media_Controller_Remote_Client.Models;
+
 namespace Media_Controller_Remote_Client.HamburgerMenuPages;
 
 public partial class VolumeMixerPage : ContentPage
 {
-	public VolumeMixerPage()
-	{
-		InitializeComponent();
-	}
+    public VolumeMixerPage()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        var viewModel = (VolumeMixerViewModel)BindingContext;
+        _ = viewModel.LoadVolumes();
+    }
+
+    void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        var slider = sender as Slider;
+        var application = slider.BindingContext as ApplicationVolume;
+        application.Volume = (float)e.NewValue;
+        var viewModel = (VolumeMixerViewModel)BindingContext;
+        viewModel.UpdateVolumeCommand.Execute(application);
+    }
+
 }
---
+++ Media Controller Remote Client/Models/VolumeMixerViewModel.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows.Input;
+using Media_Controller_Remote_Client.EventClasses;
+using Media_Controller_Remote_Client.Handlers;
+using Microsoft.Maui.Controls;
+using Newtonsoft.Json;
+
+namespace Media_Controller_Remote_Client.Models
+{
+    public class VolumeMixerViewModel
+    {
+        public ObservableCollection<ApplicationVolume> ApplicationVolumes { get; set; }
+        public ICommand UpdateVolumeCommand { get; set; }
+
+        private readonly WebSocketHandler _webSocketHandler;
+
+        public VolumeMixerViewModel()
+        {
+            try
+            {
+                ApplicationVolumes = new ObservableCollection<ApplicationVolume>();
+                UpdateVolumeCommand = new Command<ApplicationVolume>(application =>
+                {
+                    Task.Run(() => UpdateVolume(application));
+                });
+
+                _webSocketHandler = WebSocketHandler.Instance;
+                _webSocketHandler.VolumeMixerInfoReceived += VolumeMixerInfoReceived;
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine($"[VolumeMixerViewModel]: {ex}");
+            }
+        }
+
+        public async Task LoadVolumes()
+        {
+            try
+            {
+                var volumeMixerEvent = new VolumeMixerEvent
+                {
+                    VolumeMixerEventType = VolumeMixerEventType.GetApplicationVolumes
+                };
+
+                var message = JsonConvert.SerializeObject(volumeMixerEvent);
+                await _webSocketHandler.SendMessageAsync(message);
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine($"[VolumeMixerViewModel]: {ex}");
+            }
+        }
+
+        public async Task UpdateVolume(ApplicationVolume application)
+        {
+            try
+            {
+                var volumeMixerEvent = new VolumeMixerEvent
+                {
+                    VolumeMixerEventType = VolumeMixerEventType.SetApplicationVolume,
+                    ApplicationVolume = application
+                };
+
+                var message = JsonConvert.SerializeObject(volumeMixerEvent);
+                await _webSocketHandler.SendMessageAsync(message);
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine($"[VolumeMixerViewModel]: {ex}");
+            }
+        }
+
+        private void VolumeMixerInfoReceived(object sender, VolumeMixerEventArgs e)
+        {
+            try
+            {
+                var volumeMixerEvent = e.VolumeMixerEvent;
+                if (volumeMixerEvent.VolumeMixerEventType == VolumeMixerEventType.GetApplicationVolumes)
+                {
+                    ApplicationVolumes.Clear();
+                    foreach (var applicationVolume in volumeMixerEvent.ApplicationVolumes)
+                    {
+                        ApplicationVolumes.Add(applicationVolume);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine($"[VolumeMixerViewModel]: {ex}");
+            }
+        }
+    }
+}
